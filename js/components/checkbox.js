d1bab4824f485b2c48151ac2dd10c9531a2c275f
Christian <post@bitschmiede.com>
2022-04-05 09:45:00 +0200

    Checkbox hat cooles Styling

--- js/components/checkbox.js
+++ js/components/checkbox.js
@@ -11,27 +11,50 @@ const checkbox = ({
     const container = dom.create({
         classes: ['containerCheckbox'],
         parent,
+        attr:{
+            'data-checked': checked
+        }
     })
 
     // Legend 
-    dom.create({
+    const label = dom.create({
         content: legend,
         parent: container,
-        type: 'span',
+        type: 'label',
         classes: ['legend']
     })
 
     // Checkbox
     const cb = dom.create({
-        type:'input',
-        attr:{
-            type:'checkbox'
+        type: 'input',
+        attr: {
+            type: 'checkbox'
         },
-        parent:container
+        listeners:{
+            change(evt){
+                container.dataset.checked = evt.target.checked;
+                if ( callback) callback();
+            }
+        },
+        classes: ['cb'],
+        parent: label
     })
-    if(checked) cb.checked = true;
-    if(callback) cb.addEventListener('change', callback);
 
+    if (checked) cb.checked = true;
+    
+
+    // Siblings for the style
+    const switcher = dom.create({
+        classes: ['switcherContainer'],
+        parent: label,
+    })
+    
+    dom.create({
+        classes: ['knob'],
+        parent: switcher,
+    })
+
+    return container;
 
 }
 
