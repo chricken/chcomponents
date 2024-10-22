2679769bcc4be1b171a3ba34f05c82d6aae753ed
Your Name <you@example.com>
2024-10-22 15:57:25 +0200

    Range

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,22 @@
 
 import '/node_modules/chComponents/index.js';
 
+const main = document.querySelector('main');
+
+const myInput = document.createElement('ch-input');
+main.append(myInput);
+
+myInput.addEventListener('input', ({ detail: res }) => {
+    // console.log(res);
+
+})
+
+const yourInput = document.createElement('ch-range');
+yourInput.update({
+    value: 400,
+    min: 100,
+    max: 1000,
+    step: 100
+})
+main.append(yourInput)
+
